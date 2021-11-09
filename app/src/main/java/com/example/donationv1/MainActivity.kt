b52d13c115678b356ba88432164954bf9e1f963e
VuongTuanSon <tuansonvuong@gmail.com>
2021-11-09 16:15:18 +0700

    WEEK 4

--- app/src/main/java/com/example/donationv1/MainActivity.kt
+++ app/src/main/java/com/example/donationv1/MainActivity.kt
@@ -2,37 +2,22 @@ package com.example.donationv1
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
-import android.widget.Button
-import android.widget.NumberPicker
-import android.widget.ProgressBar
-import android.widget.RadioGroup
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-
 
 class Donate : AppCompatActivity() {
     private var donateButton: Button? = null
     private var paymentMethod: RadioGroup? = null
     private var progressBar: ProgressBar? = null
     private var amountPicker: NumberPicker? = null
+    private var totalDonated = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_donate)
-        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
-        val fab = findViewById<View>(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view ->
-            Snackbar.make(
-                view, "Replace with your own action",
-                Snackbar.LENGTH_LONG
-            )
-                .setAction("Action", null).show()
-        }
+//        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+//        setSupportActionBar(toolbar)
         donateButton = findViewById<View>(R.id.donateButton) as Button
         if (donateButton != null) {
             Log.v("Donate", "Really got the donate button")
@@ -42,26 +27,35 @@ class Donate : AppCompatActivity() {
         amountPicker = findViewById<View>(R.id.amountPicker) as NumberPicker
         amountPicker!!.minValue = 0
         amountPicker!!.maxValue = 1000
+        progressBar!!.max = 10000
     }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_donate, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-        return if (id == R.id.action_settings) {
-            true
-        } else super.onOptionsItemSelected(item)
-    }
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        menuInflater.inflate(R.menu.menu_donate, menu)
+//        return true
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        val id = item.itemId
+//        return if (id == R.id.action_settings) {
+//            true
+//        } else super.onOptionsItemSelected(item)
+//    }
 
     fun donateButtonPressed(view: View?) {
-        Log.v("Donate", "Donate Pressed!")
+        val amount = amountPicker!!.value
+        val radioId = paymentMethod!!.checkedRadioButtonId
+        val method = if (radioId == R.id.PayPal) "PayPal" else "Direct"
+        totalDonated = totalDonated + amount
+        progressBar!!.progress = totalDonated
+        Log.v(
+                "Donate", "Donate Pressed! with amount " + amount + ", method: " +
+                method
+        )
+        Log.v("Donate", "Current total $totalDonated")
     }
 
 
