c0d4b60c6236f588f938662843a1650885dca462
VuongTuanSon <tuansonvuong@gmail.com>
2021-11-09 15:35:51 +0700

    WEEK 3

--- app/src/main/java/com/example/donationv1/MainActivity.kt
+++ app/src/main/java/com/example/donationv1/MainActivity.kt
@@ -6,21 +6,42 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.NumberPicker
+import android.widget.ProgressBar
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 
 class Donate : AppCompatActivity() {
     private var donateButton: Button? = null
+    private var paymentMethod: RadioGroup? = null
+    private var progressBar: ProgressBar? = null
+    private var amountPicker: NumberPicker? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_donate)
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
+        val fab = findViewById<View>(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view ->
+            Snackbar.make(
+                view, "Replace with your own action",
+                Snackbar.LENGTH_LONG
+            )
+                .setAction("Action", null).show()
+        }
         donateButton = findViewById<View>(R.id.donateButton) as Button
         if (donateButton != null) {
             Log.v("Donate", "Really got the donate button")
         }
+        paymentMethod = findViewById<View>(R.id.paymentMethod) as RadioGroup
+        progressBar = findViewById<View>(R.id.progressBar) as ProgressBar
+        amountPicker = findViewById<View>(R.id.amountPicker) as NumberPicker
+        amountPicker!!.minValue = 0
+        amountPicker!!.maxValue = 1000
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -38,5 +59,10 @@ class Donate : AppCompatActivity() {
             true
         } else super.onOptionsItemSelected(item)
     }
+
+    fun donateButtonPressed(view: View?) {
+        Log.v("Donate", "Donate Pressed!")
+    }
+
 
 }
