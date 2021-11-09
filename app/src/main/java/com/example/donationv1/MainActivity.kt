953682c7c1080ead6782c74569ae3d6e9bd17282
VuongTuanSon <tuansonvuong@gmail.com>
2021-11-09 15:33:33 +0700

    WEEK 2

--- app/src/main/java/com/example/donationv1/MainActivity.kt
+++ app/src/main/java/com/example/donationv1/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.donationv1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
 
 class Donate : AppCompatActivity() {
+    private var donateButton: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_donate)
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        donateButton = findViewById<View>(R.id.donateButton) as Button
+        if (donateButton != null) {
+            Log.v("Donate", "Really got the donate button")
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_donate, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
 }
